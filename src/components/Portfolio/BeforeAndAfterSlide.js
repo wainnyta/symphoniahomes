92b90560d97c4f90c4761d83f3f58752b0739598
Wainny Ta <wainny@globalofficesoftware.com>
2021-02-05 15:05:56 -0600

    clean up

--- src/components/Portfolio/BeforeAndAfterSlide.js
+++ src/components/Portfolio/BeforeAndAfterSlide.js
@@ -1,48 +1,12 @@
-import {
-  AspectRatio,
-  Flex,
-  IconButton,
-  Box,
-  Slide,
-  ScaleFade,
-  Fade,
-  SlideFade,
-  useDiscloser,
-  Stack,
-} from '@chakra-ui/react';
-import { FaAngleLeft, FaAngleRight, FaArrowLeft } from 'react-icons/fa';
+import { AspectRatio, Box, Flex } from '@chakra-ui/react';
+import { forwardRef, useImperativeHandle, useState } from 'react';
+import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
 import { AppButton } from '../AppButton';
-import { useState, forwardRef, useImperativeHandle } from 'react';
-import { useTransition, animated } from 'react-spring';
-import { motion, AnimatePresence } from 'framer-motion';
 
 //https://stackoverflow.com/questions/37949981/call-child-method-from-parent
 export const BeforeAndAfterSlide = forwardRef((props, ref) => {
   const { contentToShow } = props;
   const [displayIndex, setDisplayIndex] = useState(0);
-  const [direction, setDirection] = useState('');
-  const [showBeforePicture, setShowBeforePicture] = useState(false);
-  const [showAfterPicture, setShowAfterPicture] = useState(true);
-  const [fadeImage, setFadeImage] = useState(true);
-
-  const pages = [
-    ({ style }) => (
-      <animated.div style={{ ...style, width: '100%' }}>
-        <img
-          src={contentToShow.src2}
-          style={{ objectFit: 'cover', width: '100%' }}
-        />
-      </animated.div>
-    ),
-    ({ style }) => (
-      <animated.div style={{ ...style, width: '100%' }}>
-        <img
-          src={contentToShow.src}
-          style={{ objectFit: 'cover', width: '100%' }}
-        />
-      </animated.div>
-    ),
-  ];
 
   const getCurrentImage = () => {
     switch (displayIndex) {
@@ -75,50 +39,6 @@ export const BeforeAndAfterSlide = forwardRef((props, ref) => {
       setShowAfterPicture(false);
       setShowBeforePicture(true);
     }
-  }
-
-  function renderImageTag() {
-    // if (showAfterPicture) {
-    //   return (
-    //     <SlideFade
-    //       in={showAfterPicture}
-    //       width={showAfterPicture ? '100%' : '0%'}
-    //     >
-    //       <img
-    //         src={contentToShow.src}
-    //         style={{ objectFit: 'cover', width: '100%' }}
-    //       />
-    //     </SlideFade>
-    //   );
-    // } else {
-    //   return (
-    //     <SlideFade
-    //       in={showBeforePicture}
-    //       width={showBeforePicture ? '100%' : '0%'}
-    //     >
-    //       <img
-    //         src={contentToShow.src2}
-    //         style={{ objectFit: 'cover', width: '100%' }}
-    //       />
-    //     </SlideFade>
-    //   );
-    // }
-    // return (
-    //   <>
-    //     <SlideFade offsetX="50px" in={showAfterPicture}>
-    //       <img
-    //         src={contentToShow.src}
-    //         style={{ objectFit: 'cover', width: '100%' }}
-    //       />
-    //     </SlideFade>
-    //     <SlideFade offsetX="50px" in={showBeforePicture}>
-    //       <img
-    //         src={contentToShow.src2}
-    //         style={{ objectFit: 'cover', width: '0%' }}
-    //       />
-    //     </SlideFade>
-    //   </>
-    // );
   }
 
   const renderImage = () => {
@@ -182,8 +102,8 @@ export const BeforeAndAfterSlide = forwardRef((props, ref) => {
     }
   };
   return (
-    <AnimatePresence>
-      <motion.div
+    <>
+      <div
         initial={{ opacity: 0.7 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0.6 }}
@@ -192,7 +112,7 @@ export const BeforeAndAfterSlide = forwardRef((props, ref) => {
         }}
       >
         {renderImage()}
-      </motion.div>
-    </AnimatePresence>
+      </div>
+    </>
   );
 });
