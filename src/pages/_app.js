c2da91e5d90a2f3fcc707bf1d0b24f62f6878d64
Wainny Ta <wainny@globalofficesoftware.com>
2021-09-25 16:05:06 -0500

    Adding Google Analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,6 +9,7 @@ import 'swiper/swiper-bundle.min.css';
 import '../../src/styles.css';
 import { useEffect } from 'react';
 import TagManager from 'react-gtm-module';
+import Script from 'next/script';
 
 const tagManagerArgs = {
   gtmId: process.env.NEXT_PUBLIC_GTM_TAG,
@@ -20,6 +21,21 @@ function MyApp({ Component, pageProps }) {
   }, []);
   return (
     <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+      />
+      <Script strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
+        `}
+      </Script>
+
       <ChakraProvider resetCSS theme={theme}>
         <ColorModeProvider
           options={{
